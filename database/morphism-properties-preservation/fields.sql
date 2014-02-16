629214da3b049e10b9c04214c6619970ad4bf1dd
Pieter Belmans <pieterbelmans@gmail.com>
2014-02-16 13:54:47 +0100

    Added the 'status' of a relation in morphism-properties-preservation
    It will be 'true', 'false', 'unknown', or something like it.

--- database/morphism-properties-preservation/fields.sql
+++ database/morphism-properties-preservation/fields.sql
@@ -1,2 +1,3 @@
 ALTER TABLE [morphism-properties-preservation-rows] ADD COLUMN tag VARCHAR;
 ALTER TABLE [morphism-properties-preservation-relations] ADD COLUMN tag VARCHAR;
+ALTER TABLE [morphism-properties-preservation-relations] ADD COLUMN status VARCHAR;
